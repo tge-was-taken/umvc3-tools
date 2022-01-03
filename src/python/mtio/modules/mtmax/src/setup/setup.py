650100a5caeff5de1c67b4a3580be182b8631fce
TGEnigma <smartychrisje@hotmail.com>
2022-01-03 01:11:34 +0100

    add option to specify max installation path manually in setup

--- src/python/mtio/modules/mtmax/src/setup/setup.py
+++ src/python/mtio/modules/mtmax/src/setup/setup.py
@@ -15,26 +15,36 @@ def isAdmin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+    
+def parseMaxVersion( name ):
+    if name.startswith( '3ds Max ' ):
+        verStr = name.replace( '3ds Max ', "" )
+        ver = None
+        try:
+            ver = int( verStr )
+        except:
+            print( f'warning: unable to determine 3ds Max installation version for {dir.path}')
+            return None
+        
+        if ver < MIN_SUPPORTED_VER:
+            # no python 3 support
+            print( f"notice: skipping incompatible 3ds Max installation (version: {ver}, {MIN_SUPPORTED_VER} or up required)" )
+            return None
+        
+        return ver
 
 def findSupportedMaxInstallDirs():
     autodeskDir = os.path.join( os.environ['ProgramFiles'], 'Autodesk' )
     installDirs = []
+    return installDirs
     for dir in os.scandir( autodeskDir ):
-        if dir.is_dir() and dir.name.startswith( '3ds Max ' ):
-            verStr = dir.name.replace( '3ds Max ', "" )
-            ver = None
-            try:
-                ver = int( verStr )
-            except:
-                print( f'warning: unable to determine 3ds Max installation version for {dir.path}')
-                continue
-            
-            if ver < MIN_SUPPORTED_VER:
-                # no python 3 support
-                print( f"notice: skipping incompatible 3ds Max installation (ver: {ver}, required: >= {MIN_SUPPORTED_VER})" )
-                continue
-            
+        if not dir.is_dir():
+            continue
+        
+        ver = parseMaxVersion( dir.name )
+        if ver != None:
             installDirs.append((dir.path, ver))
+                
     return installDirs
 
 def findPython3Dir( installDir ):
@@ -59,8 +69,22 @@ def selectMaxInstallDir():
     
     if len( installDirs ) == 0:
         print( 'no supported 3ds Max installations were found (min supported version is 2021)' )
-        sys.exit( 1 )
-    elif len( installDirs ) > 1:
+        path = input( "please enter the path to your 3ds Max installation or press enter to exit\n" )
+        if path.strip() == '':
+            sys.exit( 1 )
+        
+        ver = parseMaxVersion( os.path.basename( path ) )
+        if ver == None:
+            print( 'the specified installation directory does not contain a compatible 3ds Max installation' )
+            sys.exit( 1 )
+        
+        if ver < MIN_SUPPORTED_VER:
+            print( f"the specified 3ds Max installation version is not supported (version: {ver}, {MIN_SUPPORTED_VER} or up required)" )
+            sys.exit( 1 )
+            
+        installDirs.append( (path, ver) )
+        
+    if len( installDirs ) > 1:
         print( 'multiple compatible 3ds Max installations were detected' )
         for installDir, ver in installDirs:
             print( f'version {ver}\t{installDir}')
